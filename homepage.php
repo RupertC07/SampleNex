51be1b0aac35f575cd3ed1ca8bbd3abb9e4607c4
Rupert Caingal <johnrupertcaingal@gmail.com>
2023-08-30 22:59:58 +0800

    added a new php page

--- homepage.php
+++ homepage.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NEX HK</title>
+    <title>Document</title>
 </head>
-<body>
 
-    <h1>WELCOME TO NEX HK</h1>
-    <p>Formerly Known as HKSMMS</p>
-    
+<body>
+    <h1>This is a php page</h1>
 </body>
+
 </html>
